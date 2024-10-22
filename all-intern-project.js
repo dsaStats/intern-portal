c2e61bfccb5d4b9fcd22f95c1580bb508055ffb7
Satashee Sharma <satasheesharma@gmail.com>
2024-10-23 01:21:58 +0530

    All intern projects showcase page.

---
+++ all-intern-project.js
@@ -0,0 +1,150 @@
+gsap.from("#project_card-1", {
+    x: -600,
+    y: 0,
+    opacity: -2,
+    duration: 3,
+    scrollTrigger: {
+        trigger: ".intern-project-side h3",
+        scroller: "body"
+    }
+});
+gsap.from("#project_card-2", {
+    x: -700,
+    y: 0,
+    opacity: -2,
+    duration: 3,
+    scrollTrigger: {
+        trigger: ".intern-project-side h3",
+        scroller: "body"
+    }
+});
+gsap.from("#project_card-3", {
+    x: 0,
+    y: 500,
+    opacity: -2,
+    duration: 2.5,
+    scrollTrigger: {
+        trigger: ".intern-project-side h3",
+        scroller: "body"
+    }
+});
+gsap.from("#project_card-4", {
+    x: 0,
+    y: 500,
+    opacity: -2,
+    duration: 2.5,
+    scrollTrigger: {
+        trigger: ".intern-project-side h3",
+        scroller: "body"
+    }
+});
+gsap.from("#project_card-5", {
+    x: 700,
+    y: 0,
+    opacity: -2,
+    duration: 3,
+    scrollTrigger: {
+        trigger: ".intern-project-side h3",
+        scroller: "body"
+    }
+});
+gsap.from("#project_card-6", {
+    x: 600,
+    y: 0,
+    opacity: -2,
+    duration: 3,
+    scrollTrigger: {
+        trigger: ".intern-project-side h3",
+        scroller: "body"
+    }
+});
+gsap.from("#project_card-7", {
+    x: 500,
+    y: 0,
+    opacity: -2,
+    duration: 3,
+    scrollTrigger: {
+        trigger: ".intern-project-side h3",
+        scroller: "body"
+    }
+});
+gsap.from("#project_card-8", {
+    x: 300,
+    y: 0,
+    opacity: 1,
+    duration: 2,
+    scrollTrigger: {
+        trigger: ".intern-project-side h3",
+        scroller: "body"
+    }
+});
+gsap.from("#project_card-9", {
+    x: 300,
+    y: 0,
+    opacity: 1,
+    duration: 2,
+    scrollTrigger: {
+        trigger: ".intern-project-side h3",
+        scroller: "body"
+    }
+});
+gsap.from("#project_card-10", {
+    x: 300,
+    y: 0,
+    opacity: 1,
+    duration: 2,
+    scrollTrigger: {
+        trigger: ".intern-project-side h3",
+        scroller: "body"
+    }
+});
+gsap.from("#project_card-11", {
+    x: 300,
+    y: 0,
+    opacity: 1,
+    duration: 2,
+    scrollTrigger: {
+        trigger: ".intern-project-side h3",
+        scroller: "body"
+    }
+});
+gsap.from("#project_card-12", {
+    x: 300,
+    y: 0,
+    opacity: 1,
+    duration: 2,
+    scrollTrigger: {
+        trigger: ".intern-project-side h3",
+        scroller: "body"
+    }
+});
+gsap.from("#project_card-13", {
+    x: 300,
+    y: 0,
+    opacity: 1,
+    duration: 2,
+    scrollTrigger: {
+        trigger: ".intern-project-side h3",
+        scroller: "body"
+    }
+});
+gsap.from("#project_card-14", {
+    x: 300,
+    y: 0,
+    opacity: 1,
+    duration: 2,
+    scrollTrigger: {
+        trigger: ".intern-project-side h3",
+        scroller: "body"
+    }
+});
+gsap.from("#project_card-15", {
+    x: 300,
+    y: 0,
+    opacity: 1,
+    duration: 2,
+    scrollTrigger: {
+        trigger: ".intern-project-side h3",
+        scroller: "body"
+    }
+});
